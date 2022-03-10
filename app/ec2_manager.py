ce31056cc1466eaca61a77a62a1bc0cf249c2f5a
emrecakmak <emrecakmak.bilkent@gmail.com>
2022-03-10 21:57:07 +0300

    Try-except added

--- app/ec2_manager.py
+++ app/ec2_manager.py
@@ -21,39 +21,46 @@ def ec2action(action):
         aws_access_key_id=aws_data["aws_access_key_id"],
         aws_secret_access_key=aws_data["aws_secret_access_key"] 
     ) 
-    logging.info("AWS Client Created")
+   
+    try:
+        if (action=="list"):
+            aws_response=aws_client.describe_instances()
+            ec2_Instances=aws_response["Reservations"]
+            instanceId="InstanceIDs"
+            ec2list=[]
+            for i in ec2_Instances:
+                ec2list.append(i["Instances"][0]["InstanceId"])
+            response = make_response(
+                jsonify(instanceId=ec2list),
+                200,)
+            logging.info("List Returned.")     
+            return response             
 
-    if (action=="list"):
-        aws_response=aws_client.describe_instances()
-        ec2_Instances=aws_response["Reservations"]
-        instanceId="InstanceIDs"
-        ec2list=[]
-        for i in ec2_Instances:
-             ec2list.append(i["Instances"][0]["InstanceId"])
-        response = make_response(
-            jsonify(instanceId=ec2list),
-             200,)
-        return response             
+        elif (action=="start"):
+            insID=aws_data["InstanceId"]
+            aws_response=aws_client.start_instances(InstanceIds=[insID])
+            status="status"
+            response = make_response(
+                jsonify(status=aws_response["StartingInstances"][0]["CurrentState"]["Name"]),
+                200,)
+            logging.info("Instances Started.")     
+            return response
 
-    elif (action=="start"):
-        insID=aws_data["InstanceId"]
-        aws_response=aws_client.start_instances(InstanceIds=[insID])
-        status="status"
-        response = make_response(
-            jsonify(status=aws_response["StartingInstances"][0]["CurrentState"]["Name"]),
-             200,)
-        return response
-
-    elif (action=="stop"):
-        insID=aws_data["InstanceId"]
-        aws_response=aws_client.stop_instances(InstanceIds=[insID])
-        status="status"
-        response = make_response(
-            jsonify(status=aws_response["StoppingInstances"][0]["CurrentState"]["Name"]),
-             200,)
-        return response        
-    else:
-        return "Undefined action."
+        elif (action=="stop"):
+            insID=aws_data["InstanceId"]
+            aws_response=aws_client.stop_instances(InstanceIds=[insID])
+            status="status"
+            response = make_response(
+                jsonify(status=aws_response["StoppingInstances"][0]["CurrentState"]["Name"]),
+                200,)
+            logging.info("Instances Stopped.")
+            return response        
+        
+        else:
+            return make_response("Undefined action.",400)
+    
+    except Exception as e:
+        return make_response(str(e),412)
 
 if __name__ == "__main__":
     app.run(host=cfg.config["hostname"], port=cfg.config["port"],debug=True)
